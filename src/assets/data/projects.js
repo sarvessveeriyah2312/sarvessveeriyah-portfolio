ac2dd3b38013695044350f98fcb982b8c182cd16
Sarvess Veeriyah <sarvess.jr@outlook.com>
2022-09-12 17:29:53 +0800

    main

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -24,11 +24,11 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
+    name: 'LinkPro',
     desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
+      'Linkpro is an alternative of linktree build on Reactjs and Taliwind CSS hosted on Github Pages.',
     img: CoinTrackerImg,
-    link: '//www.google.com/',
+    link: '//sarvessveeriyah2312.github.io/linkpro/',
   },
   {
     id: uuidv4(),
